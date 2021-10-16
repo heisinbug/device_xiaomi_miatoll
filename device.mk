8cdd1986c9136050ab224a45aae40adc064f5506
Alexander Winkowski <dereference23@outlook.com>
2021-10-16 10:41:33 +0000

    miatoll: Drop QTI WFD
     * It breaks too often, I would like to use AOSP implementation instead.
    
    Change-Id: Ieab7b7ebd48e8f95243a9cda512f064c3c9f96a8

--- device.mk
+++ device.mk
@@ -467,13 +467,5 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
-# WiFi Display
-PRODUCT_PACKAGES += \
-    libnl \
-    libwfdaac_vendor
-
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 # Inherit proprietary targets
 $(call inherit-product, vendor/xiaomi/miatoll/miatoll-vendor.mk)
